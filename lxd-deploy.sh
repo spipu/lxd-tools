c5633d6d88cff0ed532288936f3ac7b67628df98
spipu <spipu45@gmail.com>
2018-05-30 00:29:54 +0200

    better scripts

--- lxd-deploy.sh
+++ lxd-deploy.sh
@@ -31,12 +31,12 @@ sudo lxc exec ${LXD_NAME} -- apt-get -qq -y install sudo ssh inetutils-ping curl
 showMessage "Configure SSH"
 sudo lxc exec ${LXD_NAME} -- sed -i "s/#Port 22/Port 22/g" /etc/ssh/sshd_config
 sudo lxc exec ${LXD_NAME} -- sed -i "s/#PermitRootLogin/PermitRootLogin/g" /etc/ssh/sshd_config
-sudo lxc exec ${LXD_NAME} -- systemctl restart ssh.service
 sudo lxc exec ${LXD_NAME} -- mkdir -p /root/.ssh
 sudo lxc file push ~/.ssh/id_rsa.pub ${LXD_NAME}/root/.ssh/authorized_keys
 sudo lxc exec ${LXD_NAME} -- chmod 700 /root/.ssh
 sudo lxc exec ${LXD_NAME} -- chmod 600 /root/.ssh/authorized_keys
 sudo lxc exec ${LXD_NAME} -- chown -R root.root /root/.ssh
+sudo lxc exec ${LXD_NAME} -- systemctl restart ssh.service
 
 showMessage "Add to KnowHost"
 if ! ssh-keygen -F ${LXD_HOST} > /dev/null ; then
