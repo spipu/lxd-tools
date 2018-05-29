802216ebe5ec9a34b49ddc50577d0d169be582ed
spipu <spipu45@gmail.com>
2018-05-30 00:25:47 +0200

    better scripts

--- lxd-init.sh
+++ lxd-init.sh
@@ -8,9 +8,9 @@ apt-get -y -qq install lxd > /dev/null
 
 lxd init 
 
-chmod -x /home/lamin/Logiciels/LXD/lxd-abstract.sh
-chmod +x /home/lamin/Logiciels/LXD/lxd-deploy.sh
-chmod +x /home/lamin/Logiciels/LXD/lxd-remove.sh
+chmod -x ${CURRENT_FOLDER}/lxd-abstract.sh
+chmod +x ${CURRENT_FOLDER}/lxd-deploy.sh
+chmod +x ${CURRENT_FOLDER}/lxd-remove.sh
 
 ln -fs ${CURRENT_FOLDER}/lxd-abstract.sh /usr/local/bin/lxd-abstract
 ln -fs ${CURRENT_FOLDER}/lxd-deploy.sh   /usr/local/bin/lxd-deploy
