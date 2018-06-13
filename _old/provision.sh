59df87ff0a843e52971988e7d1e5410cc0134b59
spipu <spipu45@gmail.com>
2018-06-13 23:25:16 +0200

    remove useless fields

--- _old/provision.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-showMessage "Install basic packages"
-sudo lxc exec $LXD_NAME -- apt-get -qq -y install inetutils-ping curl vim aptitude ca-certificates bash-completion file iproute less lsof moreutils patch rsync net-tools screen ssl-cert strace tcpdump telnet unzip ntp acpid iotop dstat apt-transport-https tar wget > /dev/null
-
