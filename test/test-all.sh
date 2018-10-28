1cebba38ae09e02352dce63b4065597f081af15d
spipu <spipu45@gmail.com>
2018-10-28 17:29:22 +0100

    add trusty

--- test/test-all.sh
+++ test/test-all.sh
@@ -4,10 +4,13 @@ set -e
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 FOLDERS=(
+    # Debian
     "debian-7-wheezy"
     "debian-8-jessie"
     "debian-9-stretch"
     "debian-10-buster"
+    # Ubuntu
+    "ubuntu-14_04-trusty"
     "ubuntu-16_04-xenial"
     "ubuntu-18_04-bionic"
 )
