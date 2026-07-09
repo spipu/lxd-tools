c03179bc06340b4d15d1810c1c2c9c60ca7f37ad
Laurent Minguet <laurent@spareka.com>
2026-07-09 16:53:08 +0200

    update templates

--- include/deploy/add-to-knowhosts.sh
+++ include/deploy/add-to-knowhosts.sh
@@ -2,14 +2,13 @@
 
 showMessage " > Add to KnowHost"
 
-if ! ssh-keygen -F $LXD_IP > /dev/null ; then
-    ssh-keyscan $LXD_IP 2> /dev/null >>  $CURRENT_USER_HOME/.ssh/known_hosts
+if ! ssh-keygen -F "$LXD_IP" > /dev/null ; then
+    ssh-keyscan "$LXD_IP" 2> >(grep -v '^#' >&2) >> "$CURRENT_USER_HOME/.ssh/known_hosts"
 fi
 
-for LXD_HOST in ${LXD_HOSTS[@]}; do
-    showMessage "  - $LXD_HOST"
-    if ! ssh-keygen -F $LXD_HOST > /dev/null ; then
-        ssh-keyscan $LXD_HOST 2> /dev/null >>  $CURRENT_USER_HOME/.ssh/known_hosts
-    fi
-done
+LXD_HOST_MAIN="${LXD_HOSTS[0]}"
 
+showMessage "  - $LXD_HOST_MAIN"
+if ! ssh-keygen -F "$LXD_HOST_MAIN" > /dev/null ; then
+    ssh-keyscan "$LXD_HOST_MAIN" 2> >(grep -v '^#' >&2) >> "$CURRENT_USER_HOME/.ssh/known_hosts"
+fi
